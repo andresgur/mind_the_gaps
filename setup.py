b442b23f7aefaf93721720da286298fb98de9d9c
Andrés Gúrpide Lasheras <38692538+andresgur@users.noreply.github.com>
2023-12-04 16:31:16 +0000

    Fixed error with LensinProfile, updated dependencies

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ from setuptools import setup
 setup(name="mind_the_gaps", author="Andres Gurpide", author_email="andres.gurpide@gmail.com",
      description="Functions to manipulate and simulate lightcurves", version="1.8",
       packages=["mind_the_gaps"], long_description="see README.md", install_requires=["stingray==1.1.2", "numpy==1.24.4",
-      "astropy==5.3.3", "numexpr==2.7.3", "emcee==3.1.4", "pyfftw==0.13.0"])
+      "astropy==5.3.3", "numexpr==2.7.3", "emcee==3.1.4", "pyfftw==0.13.0", "lmfit==1.0.3", "celerite==0.4.2", "corner==2.2.2"])
