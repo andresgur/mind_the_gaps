ef06009cb6ac6666c0b55ad18b9b867a74ac2c85
andresgur <andress.gurpide@gmail.com>
2024-05-27 14:09:42 +0100

    Changed param names to match celerite convention

--- mind_the_gaps/models/psd_models.py
+++ mind_the_gaps/models/psd_models.py
@@ -4,27 +4,27 @@ import numpy as np
 from scipy.special import gamma
 
 @custom_model
-def SHO(x, S_0=1, Q=10, w_0=1):
+def SHO(x, S0=1, Q=10, omega0=1):
     """Equation 20 from Foreman-Mackey+2017 (tested)
-    x and w_0 need to be given in the same units"""
-    return np.sqrt(2 / np.pi) * S_0 * w_0**4 / ((x**2 - w_0**2)**2 + (x**2) * (w_0**2)/Q**2)
+    x and omega0 need to be given in the same units"""
+    return np.sqrt(2 / np.pi) * S0 * omega0**4 / ((x**2 - omega0**2)**2 + (x**2) * (omega0**2)/Q**2)
 
 
 @custom_model
-def Lorentzian(x, S_0=1, Q=10, w_0=1):
+def Lorentzian(x, S0=1, Q=10, omega0=1):
     """Equation 11 from Foreman-Mackey+2017 (tested)
-    x and w_0 need to be given in the same units
+    x and omega0 need to be given in the same units
     """
-    a = S_0
-    c = w_0 / 2 / Q
-    return np.sqrt(1 / 2 / np.pi) * a/c * (1 / (1 + ((x - w_0)/c)**2) + 1 / (1 + ((x + w_0)/c)**2))
+    a = S0
+    c = omega0 / 2 / Q
+    return np.sqrt(1 / 2 / np.pi) * a/c * (1 / (1 + ((x - omega0)/c)**2) + 1 / (1 + ((x + omega0)/c)**2))
 
 
 @custom_model
-def BendingPowerlaw(x, S_0=1, w_0=1, Q=1/2):
+def BendingPowerlaw(x, S0=1, omega0=1, Q=1/2):
     """The PSD of a DampedRandomWalk. Astropy gives issues when using threads, so create it in each new thread"""
-    a = S_0
-    c = 0.5 * w_0/Q
+    a = S0
+    c = 0.5 * omega0/Q
     return np.sqrt(2 / np.pi) * a/c * (1 / (1 + (x/c)**2))
 
 @custom_model
@@ -34,8 +34,6 @@ def Matern32(x, sigma=1, rho=1):
     n = 1
     nu = 3 / 2
     return 1 / np.sqrt(2 * np.pi) * sigma**2 * 2**n * np.pi ** (n/2) * gamma(nu + n /2) * (2 *nu)**nu / (gamma(nu) * rho**(2*nu)) * (2 * nu / rho**2 + x**2) ** -(nu + n/2)
-
-
 
 @custom_model
 def Jitter(x, sigma=1):
