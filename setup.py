c0745994b9cf8510775efc08bae56db70d9c927d
andresgur <andress.gurpide@gmail.com>
2024-11-05 04:11:12 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -28,10 +28,10 @@ setup(
 
     long_description="see README.md",
     install_requires=[
-        "stingray==2.0.0rc1", "numpy==1.24.4",
-        "astropy==5.3.3", "numexpr==2.8.7",
+        "stingray==2.0.0rc1", "numpy>=1.24.4",
+        "astropy>=5.3.3", "numexpr==2.10.1",
         "emcee==3.1.4", "pyfftw==0.13.0",
-        "lmfit==1.0.3", "celerite==0.4.2", "corner==2.2.2"
+        "lmfit==1.0.3", "celerite>=0.4.2", "corner>=2.2.2"
     ],
     url="https://github.com/andresgur/mind_the_gaps"
 )
